6aa8c8535080c0670ac34227835fdc1cea4bbefa
Anwender <Anwender@DESKTOP-1OBRMCG>
2026-03-19 22:13:42 +0100

    Projektdateien hinzufügen.

---
+++ PräsentationZwei/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int result = Add(10, 3);
+
+        Console.WriteLine("Test wird ausgeführt...");
+
+        if (result == 15)
+        {
+            Console.WriteLine("Test bestanden");
+        }
+        else
+        {
+            Console.WriteLine("Fehler erkannt");
+        }
+
+        Console.ReadLine();
+    }
+
+    static int Add(int a, int b)
+    {
+        return a + b;
+    }
+}
